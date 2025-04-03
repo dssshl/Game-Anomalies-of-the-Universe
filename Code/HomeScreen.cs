9d960cbbdc64d609f6c1daf1d38dcba9562ad7be
dsshl <dsshl@DESKTOP-FDKAEH6>
2025-04-03 16:46:05 +0500

    Добавьте файлы проекта.

---
+++ Code/HomeScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game_Anomalies_of_the_Universe
+{
+    static class HomeScreen
+    {
+        static public Texture2D homeScreen {  get; set; }
+        static int timeCount = 0;
+        static Color color;
+
+        static public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(homeScreen, new Rectangle(0, 0, 1280, 768), color);
+        }
+
+        static public void Update()
+        {
+            color = Color.FromNonPremultiplied(255, 255, 255, timeCount % 256);
+            timeCount++;
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game_Anomalies_of_the_Universe
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            HomeScreen. homeScreen = Content.Load<Texture2D>("homeScreen");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            HomeScreen.Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            HomeScreen.Draw(spriteBatch);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
